ef180ce1a1947b4526159c6f111986c4b68cdfbc
Medina1 <121581992+Mediinnaa@users.noreply.github.com>
2023-07-02 19:01:56 +0200

    Create parking.java

---
+++ parking.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.util.LinkedList;
+
+
+public class car{
+    public static void main(String args[]){
+
+        LinkedList <vehicle>list1 = new LinkedList<vehicle>();
+        LinkedList <vehicle>list2 = new LinkedList<vehicle>();
+
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("");
+        System.out.println("\t"+"	*****WELCOME TO COMFORT CAR PARKING *****");
+        System.out.println("\t"+"  NAME: Prishtina Parking ");
+        System.out.println("\t"+"  Reg#: 151CS");
+        System.out.println("\t"+"  RollNo: 202,125,198 ");
+        System.out.println("");
+        System.out.println("\t"+"Car Park contains : " + list1);
+
+
+        int inputNum1=0;
+        int carNum1;
+        int Num2;
